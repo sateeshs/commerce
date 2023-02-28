8f3b2ff1960a5da732175e9521b61daf445c0f28
ssateesh <sateesh.sabbineni@hotmail.com>
2023-02-28 18:06:47 -0500

    renamed

--- types/base-next-page.ts
+++ types/base-next-page.ts
@@ -2,5 +2,5 @@ import { NextPage } from "next"
 import { type } from "os"
 
 
-type CCNextPage<type> = NextPage<type> & { auth: boolean}
-export type {CCNextPage}
+type BaseNextPage<type> = NextPage<type> & { auth: boolean}
+export type {BaseNextPage}
